9ee6207be06de40420a947881ab07dc94bee19ac
ekov1 <kristian_ekov@abv.bg>
2017-07-16 16:28:59 +0300

    .

---
+++ regular/C# - 2/00. Exams/14 Sept 2013 - Morning/01. Multiverse Communication/01. Multiverse Communication.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _01.Multiverse_Communication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input, digitIn13;
+            long decimlaRepresentation = 0, decimalValue = 0;
+
+            var alphabet = new List<string> { "CHU", "TEL", "OFT", "IVA", "EMY", "VNB",
+                "POQ", "ERI", "CAD", "K-A", "IIA", "YLO", "PLA" };
+            // string alphabetFile = @"alphabet.txt";
+
+            // StreamReader sr = new StreamReader(alphabetFile);
+
+            // using (sr)
+            // {
+            //     for (int i = 0; i < 13; i++)
+            //     {
+            //         alphabet.Add(sr.ReadLine());
+            //     }
+            // }
+
+            // for (int i = 0; i < alphabet.Count; i++)
+            // {
+            //     Console.WriteLine(alphabet[i]);
+            // }
+
+            input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i += 3)
+            {
+                digitIn13 = input.Substring(i, 3);
+                decimalValue = alphabet.IndexOf(digitIn13);
+
+                decimlaRepresentation *= 13;
+                decimlaRepresentation += decimalValue;
+            }
+
+            Console.WriteLine(decimlaRepresentation);
+        }
+    }
+}
---
+++ regular/C# - 2/00. Exams/14 Sept 2013 - Morning/02. Magic Words/02. Magic Words.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.Magic_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, newIndex,
+                maxLength = 0;
+
+            List<string> words = new List<string>();
+            StringBuilder result;
+
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                words.Add(Console.ReadLine());
+            }
+
+            // Reorder
+            for (int i = 0; i < n; i++)
+            {
+                var word = words[i];
+                newIndex = word.Length % (n + 1);
+
+                words[i] = null;
+                words.Insert(newIndex, word);
+                words.Remove(null);
+
+                //if (newIndex < i)
+                //    words.RemoveAt(i + 1);
+                //else
+                //    words.RemoveAt(i);
+            }
+
+            // Print
+            result = new StringBuilder();
+
+            // foreach (var word in words)
+            //     maxLength = Math.Max(maxLength, wr.Length);
+            maxLength = words.Max(word => word.Length);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                foreach (var word in words)
+                {
+                    if (word.Length > i)
+                        result.Append(word[i]);
+                }
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
