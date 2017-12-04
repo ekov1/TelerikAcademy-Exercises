e26ddbab6f6111b3562a2b6c61246f519a780e05
ekov1 <kristian_ekov@abv.bg>
2017-12-04 20:32:31 +0200

    C#2-1-maximal increasing sequence

---
+++ regular/C#/C# - 2/01. Arrays/homeWork/05. Maximal Increasing Sequence/05. Maximal Increasing Sequence.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Maximal_Increasing_Sequence
+{
+    class Program
+    {
+        /// <summary>
+        /// Maximal increasing sequence
+
+        ///Description
+        ///
+        ///Write a program that finds the length of the maximal increasing sequence in an array of N integers.
+        ///
+        ///Input
+        ///
+        ///On the first line you will receive the number N
+        ///On the next N lines the numbers of the array will be given
+        ///Output
+        ///
+        ///Print the length of the maximal increasing sequence
+        ///Constraints
+        ///
+        /*1 <= N <= 1024
+       Time limit: 0.1s
+       Memory limit: 16MB */
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int n;
+            int previousNum;
+            int newNum;
+            int curSeq = 1;
+            int maxSeq = 1;
+
+            n = int.Parse(Console.ReadLine());
+            previousNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                newNum = int.Parse(Console.ReadLine());
+                if (previousNum < newNum)
+                {
+                    curSeq++;
+                }
+                else
+                {
+                    if (curSeq > maxSeq)
+                    {
+                        maxSeq = curSeq;
+                    }
+                    curSeq = 1;
+                }
+                previousNum = newNum;
+            }
+
+            if (curSeq > maxSeq)
+            {
+                maxSeq = curSeq;
+            }
+
+            Console.WriteLine(maxSeq);
+        }
+    }
+}
