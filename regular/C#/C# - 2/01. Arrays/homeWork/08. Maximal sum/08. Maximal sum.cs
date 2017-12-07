b59107454c4ab1747c1281354842620514753646
ekov1 <kristian_ekov@abv.bg>
2017-12-07 18:00:47 +0200

    mvc template project

---
+++ regular/C#/C# - 2/01. Arrays/homeWork/08. Maximal sum/08. Maximal sum.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Maximal_sum
+{
+    class Program
+    {
+        /// <summary>
+        /// Maximal sum
+        /// 
+        /// Description:
+        /// Write a program that finds the maximal sum of consecutive elements in a given array of N numbers.
+        /// Can you do it with only one loop (with single scan through the elements of the array)?
+        /// 
+        /// Input:
+        /// On the first line you will receive the number N
+        /// On the next N lines the numbers of the array will be given
+        /// 
+        /// Output:
+        /// Print the maximal sum of consecutive numbers
+        /// 
+        /// Constraints:
+        //  1 <= N <= 1024
+        //  Time limit: 0.1s
+        //  Memory limit: 16MB
+        /// </summary>
+
+        //static void Main(string[] args)
+        //{
+        //    int n, answer;
+        //    int[] numbers;
+
+        //    n = int.Parse(Console.ReadLine());
+        //    numbers = new int[n];
+
+        //    // record array
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        numbers[i] = int.Parse(Console.ReadLine());
+        //    }
+
+        //    answer = MaxSumTwoLops(numbers);
+        //    Console.WriteLine(answer);
+        //}
+
+        //static int MaxSumTwoLops(int[] numbers)
+        //{
+        //    int tempSum;
+        //    int answer = 0;
+        //    int length = numbers.Length;
+
+        //    for (int i = 0; i < length; i++)
+        //    {
+        //        tempSum = numbers[i];
+        //        for (int j = i + 1; j < length; j++)
+        //        {
+        //            tempSum += numbers[j];
+        //            if (tempSum > answer)
+        //            {
+        //                answer = tempSum;
+        //            }
+        //        }
+        //    }
+
+        //    return answer;
+        //}
+    }
+}
---
+++ regular/C#/C# - 2/01. Arrays/homeWork/08. Maximal sum/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //Console.write("enter the number of elements your array will contain: "); //<----------  decomment these lines to enter your own array
+        //int n = int.Parse(Console.ReadLine());
+        //int[] arr = new int[n];
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    arr[i] = int.Parse(Console.ReadLine());
+        //}
+
+        int[] arr = { -2, -3, -6, -1, -2, -1, -6, -4, -8, -8 };
+        int currentSum = 0;
+        int maxSum = 0;
+
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            currentSum = arr[i] + currentSum;
+
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0)
+            {
+                currentSum = 0;
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("The maximal sum of a subarray is: {0}", maxSum);
+        Console.WriteLine();
+
+
+    }
+}
