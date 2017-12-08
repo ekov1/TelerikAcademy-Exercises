1cf36ccc93935d2e861fc3576b96a43d70dbb566
ekov1 <kristian_ekov@abv.bg>
2017-12-08 22:37:01 +0200

    C#2 - 2 - d -Pascal Triangle

---
+++ regular/C#/C# - 2/02. Multidimensional Arrays/demos/05. PascalTriangle/05. PascalTriangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.PascalTriangle
+{
+    class PascalTriangle
+    {
+        private static int height;
+        private static long[][] triangle;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Height");
+            height = int.Parse(Console.ReadLine());
+
+            // Alocate the array in the triangle form
+            triangle = new long[height + 1][];
+
+            for (int row = 0; row <= height; row++)
+            {
+                triangle[row] = new long[row + 1];
+            }
+
+            // Calculate the triangle
+            triangle[0][0] = 1;
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    triangle[row + 1][col] += triangle[row][col];
+                    triangle[row + 1][col + 1] += triangle[row][col];
+                }
+            }
+
+            // Print the Triangle
+            for (int row = 0; row <= height; row++)
+            {
+                Console.Write("".PadLeft((height - row) * 2));
+                for (int col = 0; col < row; col++)
+                {
+                    Console.Write("{0,3} ", triangle[row][col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ regular/C#/C# - 2/02. Multidimensional Arrays/homeWork/01. Fill the Matrix/01. Fill the Matrix.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Fill_the_Matrix
+{
+    class Program
+    {
+        static int[,] matrix;
+        static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            matrix = new int[n, n];
+            string letter = Console.ReadLine().ToLower();
+
+
+
+            switch (letter)
+            {
+                case "a":
+                    FillMatrixA();
+                    break;
+
+            }
+
+            PrintMatrix();
+
+        }
+
+        // Methods
+        private static void PrintMatrix()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j!=4)
+                    {
+                        if (i<10)
+                        {
+                            Console.Write(matrix[i, j] + "  ");
+                        }
+                        else
+                        {
+                            Console.Write(matrix[i, j] + " ");
+                        }
+                        
+                    }
+                    else
+                    {
+                        Console.Write(matrix[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillMatrixA()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = counter++;
+                }
+            }
+        }
+    }
+}
