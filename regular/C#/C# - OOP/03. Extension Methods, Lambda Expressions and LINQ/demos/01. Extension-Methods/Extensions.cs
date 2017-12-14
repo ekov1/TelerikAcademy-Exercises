ad536e6a6d5683dfde4047f0ef70e1b064b89ce4
ekov1 <kristian_ekov@abv.bg>
2017-12-14 19:34:24 +0200

    c# oop 3 - d - extencions.cs

---
+++ regular/C#/C# - OOP/03. Extension Methods, Lambda Expressions and LINQ/demos/01. Extension-Methods/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Extension_Methods
+{
+    public static class Extensions
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Split(new[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static string ToString<T>(this IEnumerable<T> enumeration)
+        {
+            var result = new StringBuilder();
+
+            result.Append("[ ");
+            foreach (var item in enumeration)
+            {
+                result.Append(item + " ");
+            }
+            result.Append("]");
+            return result.ToString();
+        }
+
+        public static void IncreaseWidth(this List<int> list, int amount)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] += amount;
+            }
+        }
+    }
+}
---
+++ regular/C#/C# - OOP/03. Extension Methods, Lambda Expressions and LINQ/demos/01. Extension-Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = "one two seven";
+            Console.WriteLine(words.WordCount()); // => 3
+
+            var x = new List<int>() { 1, 2, 3 };
+            Console.WriteLine(x.ToString<int>());
+
+            x.IncreaseWidth(3);
+            Console.WriteLine(x.ToString<int>());
+        }
+    }
+}
