94b429ff21e15372ed82e2a408a981bf15bd4415
ekov1 <kristian_ekov@abv.bg>
2017-12-14 19:54:55 +0200

    c# oop 3 - d - DelegateExample.cs

---
+++ regular/C#/C# - OOP/03. Extension Methods, Lambda Expressions and LINQ/demos/03. DelegatesExample/DelegatesExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.DelegatesExample
+{
+    class DelegatesExample
+    {
+        // Declaration of a delegate
+        public delegate void SimpleDelegate(string param);
+
+        private static void TestMethod(string param)
+        {
+            Console.WriteLine("I was called by a delegate.");
+            Console.WriteLine("I got parameter: {0}.", param);
+        }
+
+        static void Main(string[] args)
+        {
+            // Instantiate the delegate
+            SimpleDelegate d = new SimpleDelegate(TestMethod);
+
+            // The above can be written in a shorter way
+            d = TestMethod;
+
+            // Invocation of the method, pointed by delegate
+            d("test");
+        }
+
+
+    }
+}
