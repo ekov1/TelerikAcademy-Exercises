8e21755b87660d09aaa2718d82a8dcc1ff995523
ekov1 <kristian_ekov@abv.bg>
2017-12-17 17:38:24 +0200

    c#- oop - 1 - hW++

---
+++ regular/C#/C# - OOP/01. Defining Classes - Part 1/homeWork/MobileDevice/Modles/Battery.cs
@@ -0,0 +1,19 @@
+﻿using MobileDevice.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileDevice.Modles
+{
+   public class Battery
+    {
+        #region Fields
+        private string model;
+        private int hoursIdle;
+        private int hoursTalk;
+        private BatteryType batType;
+        #endregion
+    }
+}
---
+++ regular/C#/C# - OOP/01. Defining Classes - Part 1/homeWork/MobileDevice/Modles/Display.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileDevice.Modles
+{
+   public class Display
+    {
+        private int size;
+        private int numberOfColors;
+    }
+}
